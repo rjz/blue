b106e1c6bb86ed816316146f7266302ef8cf759c
rjz <rj@rjzaworski.com>
2013-10-07 21:48:59 -0700

    A simple (static) file server

--- src/examples/demo.rs
+++ src/examples/demo.rs
@@ -7,7 +7,7 @@ use std::rt::io::net::ip::{ SocketAddr, Ipv4Addr };
 use http::server::{ Config, Request, Server, ServerUtil, ResponseWriter };
 
 mod demo {
-    use blue::{ Filter, FilterResult, Pass, Send };
+    use blue::{ Filter, FilterResult, Pass, Send, Fail };
     use blue::request::{ Request, DirtyRequest, SimpleRequest };
     use blue::response::{ SimpleResponse };
 
@@ -58,6 +58,32 @@ mod demo {
             Pass(SimpleRequest::from_request(req))
         }
     }
+
+    // A naive static file server
+    pub struct StaticFilter {
+      static_dir: ~str
+    }
+
+    impl Filter<DirtyRequest, DirtyRequest, SimpleResponse> for StaticFilter {
+        fn filter (&self, req: DirtyRequest) -> FilterResult<DirtyRequest, SimpleResponse> {
+            use std::path::Path;
+            use std::io;
+            use std::os;
+
+            let path = req.get_uri().to_str();
+            let p = &Path(self.static_dir + path);
+
+            if (os::path_exists(p) && !os::path_is_dir(p)) {
+                match io::read_whole_file_str(p) {
+                    Ok(content) => Send(SimpleResponse { status: 200, body: content }),
+                    Err(err) => Fail(err)
+                }
+            }
+            else {
+                Pass(req.clone())
+            }
+        }
+    }
 }
 
 #[deriving(Clone)]
@@ -74,10 +100,13 @@ impl Server for DemoServer {
         let s = demo::FizService;
         let l = demo::LogService;
 
+        let public = demo::StaticFilter { dir: ~"static" };
+
         let res = blue::handle(httpReq)
             .using(l)
             .using(p)
-            .using(s);
+            .using(s)
+            .using(public);
 
         blue::respond(res, httpRes)
     }
